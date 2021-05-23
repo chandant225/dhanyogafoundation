19a69abc5894ca962188ba19b3e04ec60fba9104
manojbhatta <bob@example.com>
2021-05-23 12:07:10 +0545

    remove tags link from sidebar

--- resources/views/layouts/backend/sidebar.blade.php
+++ resources/views/layouts/backend/sidebar.blade.php
@@ -84,12 +84,6 @@
                                 <p>Categories</p>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Tags</p>
-                            </a>
-                        </li>
                     </ul>
                 </li>
                 <li class="nav-item">
