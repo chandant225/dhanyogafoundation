cd6f10ac7b0f6bddd84bde74805b369a4c130965
manojbhatta <bob@example.com>
2021-07-05 13:44:03 +0545

    bug fix

--- app/OurTeam.php
+++ app/OurTeam.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 
